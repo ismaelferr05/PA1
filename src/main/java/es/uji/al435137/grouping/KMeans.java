af8786da414b5a812de1bfd524030d25c589fd1b
ismaelferr05 <al435137@uji.es>
2025-03-04 11:17:01 +0100

    Practica 2 comienzo

--- src/main/java/es/uji/al435137/grouping/KMeans.java
+++ src/main/java/es/uji/al435137/grouping/KMeans.java
@@ -1,4 +1,15 @@
 package es.uji.al435137.grouping;
 
+import es.uji.al435137.reading.Table;
+
+import java.util.List;
+
 public class KMeans {
+    public KMeans(int numClusters, int numIterations, long seed) {
+    }
+    public void train(Table data) {
+    }
+    public Integer estimate(List<Double> sample) {
+
+    }
 }
