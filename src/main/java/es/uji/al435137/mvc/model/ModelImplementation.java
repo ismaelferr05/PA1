9acddd3f248cfe032099bdf903dfaa43329999a2
al435171 <al435171@uji.es>
2025-04-15 12:14:18 +0200

    puta madre wey

--- src/main/java/es/uji/al435137/mvc/model/ModelImplementation.java
+++ src/main/java/es/uji/al435137/mvc/model/ModelImplementation.java
@@ -1,18 +1,23 @@
 package es.uji.al435137.mvc.model;
 
+import es.uji.al435137.mvc.view.View;
+
 public class ModelImplementation implements Model {
+    private View vista;
+
+    public void setVista(View vista) {
+        this.vista = vista;
+    }
 
     @Override
     public void train() {
-        // Lógica para entrenar el modelo
         System.out.println("Entrenando el modelo...");
-        // Aquí puedes agregar la lógica específica de entrenamiento
+        // Lógica específica de entrenamiento
     }
 
     @Override
     public void generateMoreRecommendations() {
-        // Lógica para generar más recomendaciones
         System.out.println("Generando más recomendaciones...");
-        // Aquí puedes agregar la lógica específica para generar recomendaciones
+        // Lógica específica para generar recomendaciones
     }
 }
--- src/main/java/es/uji/al435137/mvc/view/ViewImplementation.java
+++ src/main/java/es/uji/al435137/mvc/view/ViewImplementation.java
@@ -1,4 +1,28 @@
 package es.uji.al435137.mvc.view;
 
+import es.uji.al435137.mvc.controlator.Controller;
+import es.uji.al435137.mvc.model.Model;
+import javafx.stage.Stage;
+
 public class ViewImplementation implements View {
+    private Controller controlador;
+    private Model modelo;
+
+    @Override
+    public void setControlador(Controller controlador) {
+        this.controlador = controlador;
+    }
+
+    @Override
+    public void setModelo(Model modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void createGUI(Stage stage) {
+        // Lógica para crear la interfaz gráfica
+        System.out.println("Creando la interfaz gráfica...");
+        stage.setTitle("Aplicación MVC");
+        stage.show();
+    }
 }
